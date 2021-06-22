29b61f579bad6aacd96c363dc5d9c0219634c7d4
Bhupendra Devada <monudevada1998@gmail.com>
2021-06-22 17:22:23 +0530

    proxy error resolved

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-let loc = document.getElementById("location");
-let tempIcon = document.getElementById("temp-icon")
-let tempValue = document.getElementById("temp-value");
-let tempUnit = document.getElementById("temp-unit");
-let climate = document.getElementById("climate");
+let loc = document.getElementById('location');
+let tempIcon = document.getElementById('temp-icon');
+let tempValue = document.getElementById('temp-value');
+let tempUnit = document.getElementById('temp-unit');
+let climate = document.getElementById('climate');
 let city = document.getElementById('searchTxt');
-let search = document.getElementById("search");
-let iconFile; 
-window.addEventListener("load", () => {
-  search.addEventListener('click',(e)=>{
+let search = document.getElementById('search');
+let iconFile;
+window.addEventListener('load', () => {
+  search.addEventListener('click', (e) => {
     let cityname = city.value;
-    cityname = cityname.charAt(0).toUpperCase() + cityname.slice(1)
+    cityname = cityname.charAt(0).toUpperCase() + cityname.slice(1);
     // console.log(cityname);
-    let proxy = "https://cors-anywhere.herokuapp.com/";
-      let api = `${proxy}https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=fb432b1c02b03d4b64d1edf087066a08`;
-      fetch(api)
+    // let proxy = "https://cors-anywhere.herokuapp.com/";
+
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=fb432b1c02b03d4b64d1edf087066a08`;
+    fetch(api)
       .then((response) => {
         return response.json();
       })
@@ -25,26 +26,20 @@ window.addEventListener("load", () => {
         tempValue.textContent = Math.round(feels_like - 273);
         loc.textContent = name;
         climate.textContent = main;
-        if (id<250){
-          tempIcon.src = './icons/storm.svg' ;
-        }
-        else if (id<350){
-          tempIcon.src = './icons/drizzle.svg' ;
-        }
-        else if (id<550){
-          tempIcon.src = './icons/rain.svg' ;
-        }
-        else if (id<650){
-          tempIcon.src = './icons/snow.svg' ;
-        }
-        else if (id<800){
-          tempIcon.src = './icons/atmosphere.svg' ;
-        }
-        else if (id===800){
-          tempIcon.src = './icons/sun.svg' ;
-        }
-        else if(id>800){
-          tempIcon.src = './icons/clouds.svg' ;
+        if (id < 250) {
+          tempIcon.src = './icons/storm.svg';
+        } else if (id < 350) {
+          tempIcon.src = './icons/drizzle.svg';
+        } else if (id < 550) {
+          tempIcon.src = './icons/rain.svg';
+        } else if (id < 650) {
+          tempIcon.src = './icons/snow.svg';
+        } else if (id < 800) {
+          tempIcon.src = './icons/atmosphere.svg';
+        } else if (id === 800) {
+          tempIcon.src = './icons/sun.svg';
+        } else if (id > 800) {
+          tempIcon.src = './icons/clouds.svg';
         }
         console.log(
           Math.round(temp - 273),
@@ -53,10 +48,10 @@ window.addEventListener("load", () => {
         );
         console.log(name);
       });
-    console.log(cityname,typeof(cityname));
-    e.preventDefault()
-  })
-  
+    console.log(cityname, typeof cityname);
+    e.preventDefault();
+  });
+
   let long;
   let lat;
 
@@ -64,9 +59,9 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      console.log(lat,long);
-      let proxy = "https://cors-anywhere.herokuapp.com/";
-      let api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=fb432b1c02b03d4b64d1edf087066a08`;
+      console.log(lat, long);
+      // let proxy = "https://cors-anywhere.herokuapp.com/";
+      let api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=fb432b1c02b03d4b64d1edf087066a08`;
       fetch(api)
         .then((response) => {
           return response.json();
@@ -79,26 +74,20 @@ window.addEventListener("load", () => {
           tempValue.textContent = Math.round(feels_like - 273);
           loc.textContent = name;
           climate.textContent = main;
-          if (id<250){
-            tempIcon.src = './icons/storm.svg' ;
-          }
-          else if (id<350){
-            tempIcon.src = './icons/drizzle.svg' ;
-          }
-          else if (id<550){
-            tempIcon.src = './icons/rain.svg' ;
-          }
-          else if (id<650){
-            tempIcon.src = './icons/snow.svg' ;
-          }
-          else if (id<800){
-            tempIcon.src = './icons/atmosphere.svg' ;
-          }
-          else if (id===800){
-            tempIcon.src = './icons/sun.svg' ;
-          }
-          else if(id>800){
-            tempIcon.src = './icons/clouds.svg' ;
+          if (id < 250) {
+            tempIcon.src = './icons/storm.svg';
+          } else if (id < 350) {
+            tempIcon.src = './icons/drizzle.svg';
+          } else if (id < 550) {
+            tempIcon.src = './icons/rain.svg';
+          } else if (id < 650) {
+            tempIcon.src = './icons/snow.svg';
+          } else if (id < 800) {
+            tempIcon.src = './icons/atmosphere.svg';
+          } else if (id === 800) {
+            tempIcon.src = './icons/sun.svg';
+          } else if (id > 800) {
+            tempIcon.src = './icons/clouds.svg';
           }
           console.log(
             Math.round(temp - 273),
